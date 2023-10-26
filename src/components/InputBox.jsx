9583871487efae7700c807f5e2f79cb77797ec6a
SarthakBhardwajCode <85218961+sarthakbhardwaj27@users.noreply.github.com>
2023-10-27 05:24:37 +0530

    final commit

--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -1,47 +1,59 @@
-import React from 'react'
+import React, { useId } from "react";
 
 function InputBox({
   label,
-
+  amount,
+  onAmountChange,
+  onCurrencyChange,
+  currencyOptions = [],
+  selectCurrency = "usd",
+  amountDisable = false,
+  currencyDisable = false,
   className = "",
 }) {
- const amountInputId = useId()
+  const amountInputId = useId();
 
   return (
-      <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
-          <div className="w-1/2">
-              <label htmlFor={amountInputId}  className="text-black/40 mb-2 inline-block">
-                  {label}
-              </label>
-              <input
-                  id={amountInputId}
-                  className="outline-none w-full bg-transparent py-1.5"
-                  type="number"
-                  placeholder="Amount"
-                  disabled={amountDisable}
-                  value={amount}
-                  onChange={(e) => onAmountChange && onAmountChange(Number(e.target.value))}
-              />
-          </div>
-          <div className="w-1/2 flex flex-wrap justify-end text-right">
-              <p className="text-black/40 mb-2 w-full">Currency Type</p>
-              <select
-                  className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
-                  value={selectCurrency}
-                  onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
-                  disabled={currencyDisable}
-              >
-                  
-                      {currencyOptions.map((currency) => (
-                          <option key={currency} value={currency}>
-                          {currency}
-                          </option>
-                      ))}
-              
-              </select>
-          </div>
+    <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
+      <div className="w-1/2">
+        {/* we are using useID hook to optimize our code */}
+        <label
+          htmlFor={amountInputId}
+          className="text-black/40 mb-2 inline-block"
+        >
+          {label}
+        </label>
+        <input
+          id={amountInputId}
+          className="outline-none w-full"
+          type="number"
+          placeholder="Amount"
+          disabled={amountDisable}
+          value={amount}
+          onChange={(e) =>
+            onAmountChange && onAmountChange(Number(e.target.value))
+          }
+        />
       </div>
+      <div className="w-1/2 flex flex-wrap justify-end text-right">
+        <p className="text-black/40 mb-2 w-full">Currency Type</p>
+        <select
+          className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
+          value={selectCurrency}
+          onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
+          disabled={currencyDisable}
+        >
+          {currencyOptions.map((currency) => (
+            //if we want to use loops in react, then we should use a key, this drastically improves the performance -> related to fibre
+            <option key={currency} value={currency}>
+              {currency}
+            </option>
+          ))}
+        </select>
+      </div>
+    </div>
   );
 }
 
-export default inputBox
+export default InputBox;
+//in production, we try to create another js file such as index.js in components folder, this is useful so that in our code we can just direct call index.js instead of importing different multiple components
